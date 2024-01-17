9116b587c395f20693bfe7092dafaa3902e43125
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-01-17 08:32:20 +0200

    create database and user for test purpose

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Create a database and user for testing purpose
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

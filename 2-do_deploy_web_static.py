50d7ad584d856912624ffebd7dd7d0c0a3457d59
elyton250 <nelysemathieu@gmail.com>
2024-02-04 22:50:39 +0200

    tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,32 +4,6 @@ from fabric.decorators import task
 from fabric.api import local, run, env
 import os
 import tarfile
-from datetime import datetime
-from sys import argv
-
-
-@task
-def do_pack():
-    """Create a .tgz archive from the contents of the web_static folder."""
-    src_dir = "web_static/"
-    dest_dir = "versions/"
-
-    if not os.path.exists(src_dir):
-        raise Exception("The source folder 'web_static/' doesn't exist.")
-
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = f"web_static_{timestamp}.tgz"
-
-    archive_path = os.path.join(dest_dir, file_name)
-
-    with tarfile.open(archive_path, "w:gz") as tar:
-        tar.add(src_dir, arcname=os.path.basename(src_dir))
-
-    print(f"TGZ archive created: {archive_path}")
-    return archive_path if os.path.exists(archive_path) else None
 
 
 env.hosts = ['54.160.110.173', '54.236.33.222']

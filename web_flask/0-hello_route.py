3c632c1994b4438b0a40e3b81e28c6b526c271d8
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-02-16 12:16:09 +0200

    using strict slashes = false

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""  a script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
-    return "<p>Hello HBNB!</p>"
+    """ display “Hello HBNB!” """
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":

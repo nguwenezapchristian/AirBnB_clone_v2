6433233e2116a7d1abeb69ee3ce093a10c628901
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-02-16 13:01:42 +0200

    uses escape from markupsafe

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application """
+from flask import Flask, url_for
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_C(text):
+    text_underscore_removed = text.replace('_', ' ')
+    return f"C {escape(text_underscore_removed)}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

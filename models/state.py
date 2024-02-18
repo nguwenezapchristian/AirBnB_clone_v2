8365061216f0ca219d8d33f2ef42ceac73cee6d6
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-02-18 22:20:37 +0200

    pycodestyle

--- models/state.py
+++ models/state.py
@@ -13,11 +13,11 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
     if getenv("HBNB_TYPE_STORAGE") != "db":
-            @property
-            def cities(self):
-                """Get a list of all related City objects."""
-                city_list = []
-                for city in list(models.storage.all(City).values()):
-                    if city.state_id == self.id:
-                        city_list.append(city)
-                return city_list
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list

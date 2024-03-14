5a762235a3863a1f8b943aa3028c43af5ac66d7d
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-03-14 13:45:36 +0000

    Add a route with query parameters

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,9 @@ from web_flask.0-hello_route import app
 
 
 if __name__ == "__main__":
+    from web_flask.0-hello_route import app
+    app.run()
+
+if __name__ == "__main__":
+    from web_flask.6-number_odd_or_even import app
     app.run()

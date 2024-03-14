1cca3c72dbe50c06fcf973f033ce0ac01da43fc2
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-03-14 17:38:45 +0000

    updated wsgi to serve another flask app

--- wsgi.py
+++ wsgi.py
@@ -3,13 +3,10 @@
 a file that will serve as the entry point for our application.
 This will tell our Gunicorn server how to interact with the application.
 """
-from web_flask.0-hello_route import app
+from web_flask.0-hello_route import app as hello_app
+from web_flask.6-number_odd_or_even import app as number_app
 
 
 if __name__ == "__main__":
-    from web_flask.0-hello_route import app
-    app.run()
-
-if __name__ == "__main__":
-    from web_flask.6-number_odd_or_even import app
-    app.run()
+    hello_app.run(port=5000)
+    number_app.run(port=5001)
